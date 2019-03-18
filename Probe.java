39745b1080af6fc194d2c3de98f4de53ab3bbfd5
6200279 <45390835+6200279@users.noreply.github.com>
2019-03-18 13:50:22 +0100

    Create Probe.java

---
+++ Probe.java
@@ -0,0 +1,12 @@
+public class Probe {
+    String name;
+    double mass,x , y;
+    public Probe(String name , double mass, double x , double y ){
+        this.name= name;
+        this.mass=mass;
+        this.x = x;
+        this.y = y ;
+
+
+    }
+}
