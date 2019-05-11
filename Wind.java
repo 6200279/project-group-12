223c900bab03397b628ce3d239aee86fe083265b
Mateusz Wiza <43853451+mateuszwiza@users.noreply.github.com>
2019-05-11 23:51:05 +0200

    Wind made more random

--- Wind.java
+++ Wind.java
@@ -1,9 +1,12 @@
+import java.util.Random;
+
 public class Wind {
     private double[] direction;
     private double windVelocity;
     private double timeSlice;
     private int dimension;
     private double altitude;
+    private double storedAltitude;
 
     private double[] windVector;
     private double[] roverVector;
@@ -14,21 +17,43 @@ public class Wind {
         roverVector = new double[dimension];
 
         windVelocity = 0;
+        storedAltitude = 0;
         this.timeSlice = timeSlice;
         this.dimension = dimension;
     }
 
-    public void generateWind(){
-        //generate direction using unit vector
-        if(dimension == 2) {    
-            direction[0] = -1.0;
-            direction[1] = 0.0;
+    public void generateConstantWind(){
+        if(dimension == 2) {
+            direction[0] = -0.9805;
+            direction[1] = -0.1961;
         }
-        windVelocity = 27.8;   //Wind speed in m/s
+        windVelocity = 27.8;   //Wind speed 100 km/h
+    }
+
+    //generate random direction of wind per every 1000 m of altitude and random wind speed with every step
+    public void generateWindRandomly(){
+        Random rd = new Random();
+        if(dimension == 2) {
+            if (Math.abs(altitude - storedAltitude) > 1000) {
+                if (rd.nextDouble() > 0.5) {
+                    direction[0] = (0.25 + (0.75 - 0.25) * rd.nextDouble())+0.5;
+                    System.out.println("wind direction x: " + direction[0]);
+                    direction[1] = 1 - direction[0];
+                } else {
+                    direction[0] = -(0.25 + (0.75 - 0.25) * rd.nextDouble())-0.5;
+                    System.out.println("wind direction x: " + direction[0]);
+                    direction[1] = -1 - direction[1];
+                }
+                storedAltitude = altitude;
+            }
+        }
+        windVelocity = (0.18 + (0.57 - 0.18) * rd.nextDouble())*100;
+        System.out.println("wind speed: " + windVelocity);
     }
 
     public void setWindVector(){
-        generateWind();
+        //generateConstantWind();   //uncomment for constant wind
+        generateWindRandomly();
         for(int i = 0; i < dimension; i++) {
             windVector[i] = direction[i] * windVelocity * timeSlice;
         }
