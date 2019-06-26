7588d863bb4834bef83a61431f3f35a1c709e0fd
Mateusz Wiza <43853451+mateuszwiza@users.noreply.github.com>
2019-06-26 16:02:00 +0200

    Update Rover.java

--- Rover.java
+++ Rover.java
@@ -1,14 +1,5 @@
 import java.util.LinkedList;
 
-class Point{      
-    double x,y;
-    public Point(double x, double y){ //
-        this.x = x;
-        this.y = y;
-
-
-    }
-}
 public class Rover {
 
 
